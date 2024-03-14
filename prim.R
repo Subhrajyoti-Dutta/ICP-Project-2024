b93eda30614e8a41df3a9614c316272d46abb1af
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2024-03-14 15:40:12 +0530

    prim algo

---
+++ prim.R
@@ -0,0 +1,35 @@
+library(r2r)
+
+n = 10
+edges = as.matrix(read.csv("edges.csv"))
+nodes = 1:n
+
+makeMat= function(edgeMat, n){
+  mat = matrix(0, n, n)
+  edges_num = dim(edgeMat)[1]
+  for (i in 1:edges_num){
+    mat[edgeMat[i,1],edgeMat[i,2]] = edgeMat[i,3]
+    mat[edgeMat[i,2],edgeMat[i,1]] = edgeMat[i,3]
+  }
+  return(mat)
+}
+
+isConnected = function(edgeMat, n){
+  mat = makeMat(edgeMat, n)
+  currEle = 1
+  nextExplore = c(1)
+  i = 1
+  while(i <= length(nextExplore)){
+    currEle = nextExplore[i]
+    adj = (1:n)[mat[currEle,] != 0]
+    nextExplore = c(nextExplore, adj[!(adj %in% nextExplore)])
+    i = i+1
+  }
+  if (i == n+1){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+isConnected(edges, n)
